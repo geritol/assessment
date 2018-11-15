e1ebcab1a6ccd3eb53931df3462333c730ac3437
geritol <gergo.tolnai@gmail.com>
2018-11-15 20:32:55 +0100

    refactor(spellOutNumber): moved mappings to a separate file

--- js-numerals/src/logic/spellOutNumber/spellOutNumber.js
+++ js-numerals/src/logic/spellOutNumber/spellOutNumber.js
@@ -1,58 +1,9 @@
-const singleDigitsToString = {
-  0: "zero",
-  1: "one",
-  2: "two",
-  3: "three",
-  4: "four",
-  5: "five",
-  6: "six",
-  7: "seven",
-  8: "eight",
-  9: "nine"
-}
-
-const teenNumbers = {
-  10: "ten",
-  11: "eleven",
-  12: "twelve",
-  13: "thirteen",
-  14: "fourteen",
-  15: "fifteen",
-  16: "sixteen",
-  17: "seventeen",
-  18: "eighteen",
-  19: "nineteen"
-}
-
-const tens = {
-  20: "twenty",
-  30: "thirty",
-  40: "forty",
-  50: "fifty",
-  60: "sixty",
-  70: "seventy",
-  80: "eighty",
-  90: "ninety"
-}
-
-const largeNumbers = [
-  {
-    start: 100,
-    spellOut: "hundred",
-    delimiter: " and "
-  },
-  {
-    start: 1000,
-    spellOut: "thousand",
-    delimiter: " and "
-  },
-  {
-    start: 1000000,
-    end: 1000000000,
-    spellOut: "million",
-    delimiter: ", "
-  }
-]
+import {
+  singleDigitsToString,
+  teenNumbers,
+  tens,
+  largeNumbers
+} from "./mappings"
 
 /*
  *  Returns the inputtend number in its english written form
