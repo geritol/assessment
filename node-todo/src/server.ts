26053dd3f8246668da2c75ee5bd6acec1f01d172
geritol <gergo.tolnai@gmail.com>
2018-11-27 13:06:55 +0100

    chore: added precommit linting

--- node-todo/src/server.ts
+++ node-todo/src/server.ts
@@ -1,10 +1,10 @@
-const Koa = require('koa');
+import * as Koa from 'koa';
 const app = new Koa();
 
-app.use(async ctx => {
+app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
 app.listen(3000);
 
-console.log('Server running on port 3000');
+console.log('Server running on port 3000'); // tslint:disable-line:no-console
